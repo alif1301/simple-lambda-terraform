74aa677b89247ea81041f3c8b85fd7852b5e461d
alif1301 <alif.spacework1@gmail.com>
2024-12-07 01:31:56 +0800

    new1

---
+++ resource.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "us-east-1"  # Set to the desired AWS region
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-execution-role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  function_name = "AlifLambdaFunction"
+  
+  handler = "lambda_function.lambda_handler"
+  runtime = "python3.10"
+  
+  role = aws_iam_role.lambda_role.arn
+
+  environment {
+    variables = {
+      ENV_VAR = "value"  # Optional, you can define environment variables for Lambda
+    }
+  }
+}
